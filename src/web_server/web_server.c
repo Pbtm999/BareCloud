b917c46508b634541b6c727d856b86e7d7c73163
Pedro Batista <pedro.batista.miguel9@gmail.com>
2025-09-16 22:57:52 +0100

    svg logo fix

--- src/web_server/web_server.c
+++ src/web_server/web_server.c
@@ -37,6 +37,7 @@ const char* get_mime_type(const char *path) {
     if (strstr(path, ".css"))  return "text/css";
     if (strstr(path, ".js"))   return "application/javascript";
     if (strstr(path, ".ico"))  return "image/x-icon";
+    if (strstr(path, ".svg"))  return "image/svg+xml";
     if (strstr(path, ".png"))  return "image/png";
     if (strstr(path, ".jpg") || strstr(path, ".jpeg")) return "image/jpeg";
     return "application/octet-stream";
